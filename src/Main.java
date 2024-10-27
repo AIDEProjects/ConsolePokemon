6c76938cb0af7f5b02a8a8252662afb55d1aa330
shikeik <2667097598@qq.com>
2024-10-27 17:22:36 +0800

    init

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,23 @@
 import java.util.*;
+import consolepokemon.core.Yabi;
 
 public class Main {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
-        
+		new Main().run();
     }
+	
+	public void run(){
+		YabiDemo();
+	}
+	
+	public void YabiDemo(){
+		Yabi yabi = new Yabi();
+		System.out.println(
+			"亚比名字："+yabi.name
+			+"\n"+"亚比最大血量："+yabi.maxHealth
+			+"\n"+"亚比血量："+yabi.health
+		);
+	}
     
 }
---
+++ src/consolepokemon/core/Yabi.java
@@ -0,0 +1,14 @@
+package consolepokemon.core;
+
+public class Yabi
+{
+	public String name="Yabi";
+	public float health;
+	public float maxHealth=20;
+	
+	public float atk=5;
+	
+	public Yabi(){
+		health = maxHealth;
+	}
+}
